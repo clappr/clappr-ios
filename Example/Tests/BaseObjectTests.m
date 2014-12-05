b30e8007927b472af1dc554c8bfbeefc5b6a2b87
Gustavo Barbosa <gustavocsb@gmail.com>
2014-12-05 16:00:35 -0200

    Add "once" method to baseObject.

--- Example/Tests/BaseObjectTests.m
+++ Example/Tests/BaseObjectTests.m
@@ -11,83 +11,141 @@
 
 SPEC_BEGIN(BaseObject)
 
-describe(@"Callback", ^{
+describe(@"BaseObject", ^{
 
     __block CLPBaseObject *baseObject;
-    __block BOOL callbackWasCalled;
 
     beforeEach(^{
         baseObject = [CLPBaseObject new];
-        callbackWasCalled = NO;
     });
 
-    it(@"should be called on event trigger", ^{
-        [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
-            callbackWasCalled = YES;
-        }];
-        
-        [baseObject trigger:@"some-event"];
-
-        [[theValue(callbackWasCalled) should] equal:theValue(YES)];
-    });
-
-    it(@"should be called for every callback registered", ^{
-        [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
-            callbackWasCalled = YES;
-        }];
-
-        __block BOOL anotherCallbackWasCalled = NO;
-        [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
-            anotherCallbackWasCalled = YES;
-        }];
-
-        [baseObject trigger:@"some-event"];
-
-        [[theValue(callbackWasCalled) should] equal:theValue(YES)];
-        [[theValue(anotherCallbackWasCalled) should] equal:theValue(YES)];
-    });
-
-    it(@"should not raise an exception if it is nil", ^{
+    it(@"should not raise an exception if its callback is nil", ^{
         [baseObject on:@"some-event" callback:nil];
-
         [[theBlock(^{ [baseObject trigger:@"some-event"]; }) shouldNot] raise];
     });
 
-    it(@"should not be called for another event trigger", ^{
-        [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
-            callbackWasCalled = YES;
-        }];
+    describe(@"on", ^{
 
-        [baseObject trigger:@"another-event"];
+        __block BOOL callbackWasCalled;
 
-        [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+        beforeEach(^{
+            callbackWasCalled = NO;
+        });
+
+        it(@"callback should be called on event trigger", ^{
+            [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            }];
+
+            [baseObject trigger:@"some-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(YES)];
+        });
+
+        it(@"callback should be called for every callback registered", ^{
+            [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            }];
+
+            __block BOOL anotherCallbackWasCalled = NO;
+            [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
+                anotherCallbackWasCalled = YES;
+            }];
+
+            [baseObject trigger:@"some-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(YES)];
+            [[theValue(anotherCallbackWasCalled) should] equal:theValue(YES)];
+        });
+
+        it(@"callback should not be called for another event trigger", ^{
+            [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            }];
+
+            [baseObject trigger:@"another-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+        });
+
+        it(@"callback should not be called for another context object", ^{
+            CLPBaseObject *anotherObject = [CLPBaseObject new];
+            [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            }];
+
+            [anotherObject trigger:@"some-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+        });
+
+        it(@"callback should not be called when handler is removed", ^{
+            EventCallback callback = ^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            };
+
+            [baseObject on:@"some-event" callback:callback];
+            [baseObject off:@"some-event" callback:callback];
+
+            [baseObject trigger:@"some-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+        });
+
+        it(@"callback should not be called if removed, but the others should", ^{
+            EventCallback callback = ^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            };
+
+            __block BOOL anotherCallbackWasCalled = NO;
+            EventCallback anotherCallback = ^(NSDictionary *userInfo) {
+                anotherCallbackWasCalled = YES;
+            };
+
+            [baseObject on:@"some-event" callback:callback];
+            [baseObject on:@"some-event" callback:anotherCallback];
+
+            [baseObject off:@"some-event" callback:callback];
+
+            [baseObject trigger:@"some-event"];
+            
+            [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+            [[theValue(anotherCallbackWasCalled) should] equal:theValue(YES)];
+        });
+
     });
 
-    it(@"should not be called for another context object", ^{
-        CLPBaseObject *anotherObject = [CLPBaseObject new];
-        [baseObject on:@"some-event" callback:^(NSDictionary *userInfo) {
-            callbackWasCalled = YES;
-        }];
+    describe(@"once", ^{
 
-        [anotherObject trigger:@"some-event"];
+        it(@"callback should be called on event trigger", ^{
+            __block BOOL callbackWasCalled = NO;
+            EventCallback callback = ^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            };
 
-        [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+            [baseObject once:@"some-event" callback:callback];
+
+            [baseObject trigger:@"some-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(YES)];
+        });
+
+        it(@"callback should not be called twice", ^{
+            __block BOOL callbackWasCalled = NO;
+            EventCallback callback = ^(NSDictionary *userInfo) {
+                callbackWasCalled = YES;
+            };
+
+            [baseObject once:@"some-event" callback:callback];
+            [baseObject trigger:@"some-event"];
+
+            callbackWasCalled = NO;
+
+            [baseObject trigger:@"some-event"];
+
+            [[theValue(callbackWasCalled) should] equal:theValue(NO)];
+        });
     });
-
-    it(@"should not be called when handler is removed", ^{
-        EventCallback callback = ^(NSDictionary *userInfo) {
-            callbackWasCalled = YES;
-        };
-
-        [baseObject on:@"some-event" callback:callback];
-        [baseObject off:@"some-event" callback:callback];
-
-        [baseObject trigger:@"some-event"];
-
-        [[theValue(callbackWasCalled) should] equal:theValue(NO)];
-    });
-
-    pending(@"should not be called if removed, but the others should", ^{});
 });
 
 SPEC_END
--- Pod/Classes/Clappr/CLPBaseObject.m
+++ Pod/Classes/Clappr/CLPBaseObject.m
@@ -37,15 +37,39 @@
     [eventHandlers[eventName] addObject:callback];
 }
 
+- (void)once:(NSString *)eventName callback:(EventCallback)callback
+{
+    __weak typeof(self) weakSelf = self;
+
+    __weak __block EventCallback blockSelf;
+
+    EventCallback wrapperCallback = [^(NSDictionary *userInfo) {
+        if (callback)
+            callback(userInfo);
+
+        [weakSelf off:eventName callback:blockSelf];
+    } copy];
+
+    blockSelf = wrapperCallback;
+
+    [self on:eventName callback:wrapperCallback];
+}
+
 - (void)off:(NSString *)eventName callback:(EventCallback)callback
 {
     if (!callback)
         return;
 
+    BOOL callbackWasFound = NO;
     for (EventCallback c in eventHandlers[eventName]) {
-        if (c == callback)
-            [eventHandlers[eventName] removeObject:callback];
+        if (c == callback) {
+            callbackWasFound = YES;
+            break;
+        }
     }
+
+    if (callbackWasFound)
+        [eventHandlers[eventName] removeObject:callback];
 }
 
 - (void)trigger:(NSString *)eventName
