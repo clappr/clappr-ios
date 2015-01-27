4382de4a568f1ea9a02c5e022d5d04a1679ac48b
Gustavo Barbosa <gustavocsb@gmail.com>
2015-01-27 11:23:57 -0200

    Fix pod structure.

---
+++ Pod/Classes/Views/CLPScrubber.h
@@ -0,0 +1,13 @@
+//
+//  CLPScrubber.h
+//  Clappr
+//
+//  Created by Gustavo Barbosa on 1/27/15.
+//  Copyright (c) 2014 globo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLPScrubber : UIView
+
+@end
---
+++ Pod/Classes/Views/CLPScrubber.m
@@ -0,0 +1,13 @@
+//
+//  CLPScrubber.m
+//  Clappr
+//
+//  Created by Gustavo Barbosa on 1/27/15.
+//  Copyright (c) 2014 globo.com. All rights reserved.
+//
+
+#import "CLPScrubber.h"
+
+@implementation CLPScrubber
+
+@end
