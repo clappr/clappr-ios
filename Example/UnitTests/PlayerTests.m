ae77708dd3519cc3e857eab0699751ed70f6a4c8
Gustavo Barbosa <gustavocsb@gmail.com>
2015-03-16 18:47:36 -0300

    player: add some tests

--- Example/UnitTests/PlayerTests.m
+++ Example/UnitTests/PlayerTests.m
@@ -4,12 +4,33 @@ SPEC_BEGIN(PlayerSpec)
 
 describe(@"Player", ^{
 
-    it(@"can be instantiated passing a source URL", ^{
-        NSURL *sourceURL = [NSURL URLWithString:@"http://www.somesite.com/somevideo.mp4"];
-        CLPPlayer *player = [[CLPPlayer alloc] initWithSourceURL:sourceURL];
+    describe(@"instantiation", ^{
 
-        CLPContainer *mainContainer = player.core.containers.firstObject;
-        [[mainContainer.playback.url should] equal:sourceURL];
+        it(@"can be instantiated passing a source URL", ^{
+            NSURL *sourceURL = [NSURL URLWithString:@"http://www.somesite.com/somevideo.mp4"];
+            CLPPlayer *player = [[CLPPlayer alloc] initWithSourceURL:sourceURL];
+
+            CLPContainer *mainContainer = player.core.containers.firstObject;
+            [[mainContainer.playback.url should] equal:sourceURL];
+        });
+
+        it(@"can be instantiated without sources. its not common, but it is ok", ^{
+            CLPPlayer *player = [[CLPPlayer alloc] init];
+            [[player.core.containers should] equal:@[]];
+        });
+    });
+
+    describe(@"source", ^{
+
+        it(@"should load sources in core after set source url", ^{
+
+            NSURL *sourceURL = [NSURL URLWithString:@"http://www.somesite.com/somevideo.mp4"];
+
+            CLPPlayer *player = [CLPPlayer new];
+
+            [[player.core should] receive:@selector(loadSources:) withArguments:@[sourceURL]];
+            player.sourceURL = sourceURL;
+        });
     });
 });
 
