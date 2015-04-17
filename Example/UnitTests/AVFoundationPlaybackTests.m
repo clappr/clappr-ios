be5edd35fb7c1e34a7c3d8b3f00bf9934eaeea6e
Gustavo Barbosa <gustavocsb@gmail.com>
2015-04-17 18:34:08 -0300

    playback: add is_playing tests

--- Example/UnitTests/AVFoundationPlaybackTests.m
+++ Example/UnitTests/AVFoundationPlaybackTests.m
@@ -8,23 +8,41 @@ describe(@"AVFoundationPlayback", ^{
     describe(@"controls", ^{
 
         __block CLPAVFoundationPlayback *playback;
+        __block AVPlayer *avPlayer;
 
         beforeEach(^{
             NSURL *url = [NSURL URLWithString:@""];
             playback = [[CLPAVFoundationPlayback alloc] initWithURL:url];
+            avPlayer = [playback valueForKey:@"_avPlayer"];
         });
 
-        it(@"should call avplayer play method for its play", ^{
-            AVPlayer *avPlayer = [playback valueForKey:@"_avPlayer"];
-            [[avPlayer should] receive:@selector(play)];
-            [playback play];
+        describe(@"play", ^{
+            it(@"should call avplayer play method for its play", ^{
+                [[avPlayer should] receive:@selector(play)];
+                [playback play];
+            });
         });
 
-        it(@"should call avplayer pause method for its pause", ^{
-            AVPlayer *avPlayer = [playback valueForKey:@"_avPlayer"];
-            [[avPlayer should] receive:@selector(pause)];
-            [playback pause];
+        describe(@"pause", ^{
+            it(@"should call avplayer pause method for its pause", ^{
+                [[avPlayer should] receive:@selector(pause)];
+                [playback pause];
+            });
         });
+
+        describe(@"is playing", ^{
+
+            it(@"should return true if avplayer's rate is positive", ^{
+                [avPlayer stub:@selector(rate) andReturn:theValue(23.0)];
+                [[theValue([playback isPlaying]) should] beTrue];
+            });
+
+            it(@"should return false if avplayer's rate is zero", ^{
+                [avPlayer stub:@selector(rate) andReturn:theValue(0.0)];
+                [[theValue([playback isPlaying]) should] beFalse];
+            });
+        });
+
     });
 });
 
