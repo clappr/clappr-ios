c2273f54a10533c9c14d39fe52fc918e120ab276
Diego Marcon <diego.marcon@corp.globo.com>
2015-11-04 16:49:40 -0200

    Add Event Protocol

---
+++ Pod/Classes/Base/EventProtocol.swift
@@ -0,0 +1,12 @@
+protocol EventProtocol {
+    func on(eventName:String, callback: EventCallback)
+    func once(eventName:String, callback: EventCallback)
+    func off(eventName:String, callback: EventCallback)
+    
+    func trigger(eventName:String)
+    func trigger(eventName:String, userInfo: [NSObject : AnyObject]?)
+    
+    func startListening(contextObject: EventProtocol, eventName: String, callback: EventCallback)
+    func stopListening()
+    func stopListening(contextObject: EventProtocol, eventName: String, callback: EventCallback)
+}
