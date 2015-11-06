1aaa8c0c35811c50d5aa5dd34d01493a2c33b7ee
Diego Marcon <diego.marcon@corp.globo.com>
2015-11-06 11:08:32 -0200

    refactor: extract common test callback variable

--- Example/Tests/BaseObjectTests.swift
+++ Example/Tests/BaseObjectTests.swift
@@ -3,13 +3,17 @@ import Nimble
 import Clappr
 
 class BaseObjectTests: QuickSpec {
-    private let eventName = "some-event"
     
     override func spec() {
         describe("BaseObject") {
             
             var baseObject: BaseObject!
             var callbackWasCalled: Bool!
+            
+            let eventName = "some-event"
+            let callback: EventCallback = { userInfo in
+                callbackWasCalled = true
+            }
             
             beforeEach {
                 baseObject = BaseObject()
@@ -18,47 +22,40 @@ class BaseObjectTests: QuickSpec {
             
             describe("on") {
                 it("Callback should be called on event trigger") {
-                    baseObject.on(self.eventName) { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
-                    baseObject.trigger(self.eventName)
+                    baseObject.on(eventName, callback: callback)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == true
                 }
                 
                 it("Callback should receive userInfo on trigger with params") {
                     var value = "Not Expected"
-                    baseObject.on(self.eventName) { userInfo in
+                    baseObject.on(eventName) { userInfo in
                         value = userInfo?["new_value"] as! String
                     }
                     
-                    baseObject.trigger(self.eventName, userInfo: ["new_value": "Expected"])
+                    baseObject.trigger(eventName, userInfo: ["new_value": "Expected"])
                     
                     expect(value) == "Expected"
                     
                 }
                 
                 it("Callback should be called for every callback registered") {
-                    baseObject.on(self.eventName) { userInfo in
-                        callbackWasCalled = true
-                    }
+                    baseObject.on(eventName, callback: callback)
                     
                     var secondCallbackWasCalled = false
-                    baseObject.on(self.eventName) { userInfo in
+                    baseObject.on(eventName) { userInfo in
                         secondCallbackWasCalled = true
                     }
                     
-                    baseObject.trigger(self.eventName)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == true
                     expect(secondCallbackWasCalled) == true
                 }
                 
                 it("Callback should not be called for another event trigger") {
-                    baseObject.on("some-event") { userInfo in
-                        callbackWasCalled = true
-                    }
+                    baseObject.on(eventName, callback: callback)
                     
                     baseObject.trigger("another-event")
                     
@@ -68,33 +65,23 @@ class BaseObjectTests: QuickSpec {
                 it("Callback should not be called for another context object") {
                     let anotherObject = BaseObject()
                     
-                    baseObject.on(self.eventName) { userInfo in
-                        callbackWasCalled = true
-                    }
+                    baseObject.on(eventName, callback: callback)
                     
-                    anotherObject.trigger(self.eventName);
+                    anotherObject.trigger(eventName);
                     
                     expect(callbackWasCalled) == false
                 }
                 
                 it("Callback should not be called when handler is removed") {
-                    let callback: EventCallback = { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
-                    baseObject.on(self.eventName, callback: callback)
-                    baseObject.off(self.eventName, callback: callback)
-                    baseObject.trigger(self.eventName)
+                    baseObject.on(eventName, callback: callback)
+                    baseObject.off(eventName, callback: callback)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == false
                 }
                 
                 it("Callback should not be called when stop listening is called") {
-                    let callback: EventCallback = { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
-                    baseObject.on(self.eventName, callback: callback)
+                    baseObject.on(eventName, callback: callback)
                     baseObject.on("another-event", callback: callback)
                     baseObject.stopListening()
                     
@@ -104,23 +91,18 @@ class BaseObjectTests: QuickSpec {
             
             describe("once") {
                 it("Callback should be called on event trigger") {
-                    baseObject.once(self.eventName) { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
-                    baseObject.trigger(self.eventName)
+                    baseObject.once(eventName, callback: callback)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == true
                 }
                 
                 it("Callback should not be called twice") {
-                    baseObject.once(self.eventName) { userInfo in
-                        callbackWasCalled = true
-                    }
+                    baseObject.once(eventName, callback: callback)
                     
-                    baseObject.trigger(self.eventName)
+                    baseObject.trigger(eventName)
                     callbackWasCalled = false
-                    baseObject.trigger(self.eventName)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == false
                 }
@@ -130,11 +112,8 @@ class BaseObjectTests: QuickSpec {
                 it("Should fire callback for an event on a given context object") {
                     let contextObject = BaseObject()
                     
-                    baseObject.listenTo(contextObject, eventName: self.eventName) { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
-                    baseObject.trigger(self.eventName)
+                    baseObject.listenTo(contextObject, eventName: eventName, callback: callback)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == true
                 }
@@ -142,32 +121,24 @@ class BaseObjectTests: QuickSpec {
             
             describe("off") {
                 it("Callback should not be called if removed") {
-                    let callback: EventCallback = { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
-                    baseObject.on(self.eventName, callback: callback)
-                    baseObject.off(self.eventName, callback: callback)
-                    baseObject.trigger(self.eventName)
+                    baseObject.on(eventName, callback: callback)
+                    baseObject.off(eventName, callback: callback)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == false
                     
                 }
                 it("Callback should not be called if removed, but the others should") {
-                    let callback: EventCallback = { userInfo in
-                        callbackWasCalled = true
-                    }
-                    
                     var anotherCallbackWasCalled = false
                     let anotherCallback: EventCallback = { userInfo in
                         anotherCallbackWasCalled = true
                     }
                     
-                    baseObject.on(self.eventName, callback: callback)
-                    baseObject.on(self.eventName, callback: anotherCallback)
+                    baseObject.on(eventName, callback: callback)
+                    baseObject.on(eventName, callback: anotherCallback)
                     
-                    baseObject.off(self.eventName, callback: callback)
-                    baseObject.trigger(self.eventName)
+                    baseObject.off(eventName, callback: callback)
+                    baseObject.trigger(eventName)
                     
                     expect(callbackWasCalled) == false
                     expect(anotherCallbackWasCalled) == true
