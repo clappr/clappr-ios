d9817a50ed5bdb776d39a0beeb9fde90577bb09b
Marcon <diego.marcon@corp.globo.com>
2015-11-09 10:50:02 -0200

    Base: Add UIBaseObject
    Swift doesn't allow forwarding invocation so all protocol methods were forwarded manually.
    This should be refactored if we find a better way of doint it.

---
+++ Pod/Classes/Base/UIBaseObject.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+public
+class UIBaseObject: UIView, EventProtocol {
+    private let baseObject = BaseObject()
+    
+    public func on(eventName:String, callback: EventCallback) {
+        baseObject.on(eventName, callback: callback)
+    }
+    
+    public func once(eventName:String, callback: EventCallback) {
+        baseObject.once(eventName, callback: callback)
+    }
+    
+    public func off(eventName:String, callback: EventCallback) {
+        baseObject.off(eventName, callback: callback)
+    }
+    
+    public func trigger(eventName:String) {
+        baseObject.trigger(eventName)
+    }
+    
+    public func trigger(eventName:String, userInfo: [NSObject : AnyObject]?) {
+        baseObject.trigger(eventName, userInfo: userInfo)
+    }
+    
+    public func listenTo(contextObject: BaseObject, eventName: String, callback: EventCallback) {
+        baseObject.listenTo(contextObject, eventName: eventName, callback: callback)
+    }
+    
+    public func stopListening() {
+        baseObject.stopListening()
+    }
+    
+    public func stopListening(contextObject: BaseObject, eventName: String, callback: EventCallback) {
+        baseObject.stopListening(contextObject, eventName: eventName, callback: callback)
+    }
+    
+    public func getEventContextObject() -> BaseObject {
+        return baseObject
+    }
+}
