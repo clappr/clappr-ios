3ccf3669a1aaa55ce8688c25f147f849e09752fb
Marcon <diego.marcon@corp.globo.com>
2015-11-23 16:33:05 -0200

    Refactor: Remove constructor from core plugin

--- Pod/Classes/Plugin/Core/UICorePlugin.swift
+++ Pod/Classes/Plugin/Core/UICorePlugin.swift
@@ -1,13 +1,3 @@
 public class UICorePlugin: UIPlugin {
-    public private(set) weak var core: Core?
-    
-    public required init(core: Core) {
-        self.core = core
-        super.init(frame: CGRectZero)
-        core.addSubview(self)
-    }
-    
-    public required init?(coder aDecoder: NSCoder) {
-        fatalError("Use init(core: Core) instead")
-    }
+    public weak var core: Core?
 }
