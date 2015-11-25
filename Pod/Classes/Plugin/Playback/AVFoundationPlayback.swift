f792c461054972a3a4c4aca34ea51fa916ced71d
Marcon <diego.marcon@corp.globo.com>
2015-11-25 14:37:30 -0200

    AVFoundationPlayback: Add basic plugin

---
+++ Pod/Classes/Plugin/Playback/AVFoundationPlayback.swift
@@ -0,0 +1,33 @@
+import AVFoundation
+
+public class AVFoundationPlayback: Playback {
+    private var avPlayer: AVPlayer!
+    private var avPlayerLayer: AVPlayerLayer!
+    
+    public required init(url: NSURL) {
+        super.init(url: url)
+        setupPlayer()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Use init(url: NSURL) instead")
+    }
+    
+    private func setupPlayer() {
+        avPlayer = AVPlayer(URL: url)
+        avPlayerLayer = AVPlayerLayer(player: avPlayer)
+        self.layer.addSublayer(avPlayerLayer)
+    }
+    
+    public override func play() {
+        avPlayer.play()
+    }
+    
+    public override func pause() {
+        avPlayer.pause()
+    }
+    
+    public override class func canPlay(url: NSURL) -> Bool {
+        return true
+    }
+}
