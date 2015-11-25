2b93ebfafa9d06e7c60ecebe8f28c28ec645bf4b
Marcon <diego.marcon@corp.globo.com>
2015-11-25 10:58:32 -0200

    Player: Add player class

---
+++ Pod/Classes/Base/Player.swift
@@ -0,0 +1,17 @@
+public class Player {
+    public private(set) var sources: [NSURL]
+    public private(set) var core: Core
+    
+    public convenience init(source: NSURL) {
+        self.init(sources: [source])
+    }
+    
+    public init(sources: [NSURL]) {
+        self.sources = sources
+        self.core = CoreFactory(sources: sources).create()
+    }
+    
+    public func attachTo(view: UIView) {
+        view.addSubview(core)
+    }
+}
