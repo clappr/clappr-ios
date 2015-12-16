5ab6cb86b4ed9979737429980ac3a714599e8ec7
Marcon <diego.marcon@corp.globo.com>
2015-12-16 11:47:16 -0200

    UIViewExtension: Extract add matching constraints

--- Pod/Classes/Extension/UIViewExtension.swift
+++ Pod/Classes/Extension/UIViewExtension.swift
@@ -1,9 +1,11 @@
 extension UIView {
     func addSubviewMatchingContraints(view: UIView) {
-        
         view.translatesAutoresizingMaskIntoConstraints = false
         addSubview(view)
-        
+        addMatchingConstraints(view)
+    }
+    
+    func addMatchingConstraints(view: UIView) {
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view" : view]))
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view" : view]))
     }
