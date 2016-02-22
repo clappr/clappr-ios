5e46913da3f8beba371a1570dd25bbdb1f1661a1
Marcon <diego.marcon@corp.globo.com>
2016-02-22 09:38:16 -0300

    Core: Instantiate media control from loader

--- Pod/Classes/Base/Loader.swift
+++ Pod/Classes/Base/Loader.swift
@@ -2,10 +2,12 @@ public class Loader {
     public var playbackPlugins: [AnyClass]
     public var containerPlugins: [AnyClass]
     public var corePlugins: [AnyClass]
+    public var mediaControl: MediaControl.Type
     
     public init() {
         playbackPlugins = [AVFoundationPlayback.self]
         containerPlugins = [PosterPlugin.self, LoadingContainerPlugin.self]
         corePlugins = []
+        mediaControl = MediaControl.self
     }
 }
