64bf0ae0f2c4adaf312c23cc57d49c329f9cf756
Marcon <diego.marcon@corp.globo.com>
2016-03-17 11:05:10 -0300

    Player: Add options to loader

--- Pod/Classes/Base/Player.swift
+++ Pod/Classes/Base/Player.swift
@@ -2,7 +2,7 @@ public class Player {
     public private(set) var core: Core
     
     public init(options: Options = [:], externalPlugins: [Plugin.Type] = []) {
-        let loader = Loader(externalPlugins: externalPlugins)
+        let loader = Loader(externalPlugins: externalPlugins, options: options)
         self.core = CoreFactory.create(loader , options: options)
     }
     
