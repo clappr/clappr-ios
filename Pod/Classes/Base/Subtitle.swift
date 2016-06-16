3fa8d43492f96ad332ab31df1ce7470debb183ba
Marcon <diego.marcon@corp.globo.com>
2016-06-16 14:52:54 -0300

    Tracks: Add init with empty raw value

--- Pod/Classes/Base/Subtitle.swift
+++ Pod/Classes/Base/Subtitle.swift
@@ -4,7 +4,7 @@ public class Subtitle {
     public var name: String?
     public var raw: AnyObject?
     
-    init(name: String, raw: AnyObject) {
+    init(name: String, raw: AnyObject? = nil) {
         self.name = name
         self.raw = raw
     }
