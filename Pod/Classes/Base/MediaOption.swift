578acc6ba161d220162ec3065751693931f38653
Marcon <diego.marcon@corp.globo.com>
2016-06-27 21:14:54 -0300

    MediaOption: Add language property

--- Pod/Classes/Base/MediaOption.swift
+++ Pod/Classes/Base/MediaOption.swift
@@ -4,10 +4,12 @@ public class MediaOption {
     public var name: String
     public var type: MediaOptionType
     public var raw: AnyObject?
+    public var language: String
     
-    init(name: String, type: MediaOptionType, raw: AnyObject? = nil) {
+    init(name: String, type: MediaOptionType, language: String, raw: AnyObject? = nil) {
         self.name = name
         self.type = type
+        self.language = language
         self.raw = raw
     }
 }
