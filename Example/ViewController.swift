178a3518cc6dbcb7eb2b9ab190c25de0e030fea6
Bruno Pinheiro <brunobp07@gmail.com>
2017-03-28 15:22:22 -0300

    feat(background_airplay): enable background external playback
    This change makes possible to keep the external playback running while
    the app is on background mode

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,7 +8,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let options = [kSourceUrl : "http://clappr.io/highline.mp4", kPosterUrl : "http://clappr.io/poster.png"]
+        let options = [
+          kSourceUrl : "http://clappr.io/highline.mp4",
+          kPosterUrl : "http://clappr.io/poster.png"
+        ]
         player = Player(options: options)
         
         listenToPlayerEvents()
