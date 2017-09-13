d925f0d56e9d640e01eee9c4bf7f6f214f8ed56e
Igor Oliveira <ioliveir@thoughtworks.com>
2017-09-13 11:19:25 -0300

    feat(loader): do not display LoadingContainerPlugin on tvOS

--- Clappr/Classes/Base/Loader.swift
+++ Clappr/Classes/Base/Loader.swift
@@ -1,6 +1,11 @@
 open class Loader {
     internal(set) open var playbackPlugins: [Plugin.Type] = [AVFoundationPlayback.self, NoOpPlayback.self]
+    #if os(iOS)
     internal(set) open var containerPlugins: [Plugin.Type] = [LoadingContainerPlugin.self]
+    #else
+    internal(set) open var containerPlugins: [Plugin.Type] = []
+    #endif
+
     internal(set) open var corePlugins = [Plugin.Type]()
     internal(set) open var mediaControl: MediaControl.Type = MediaControl.self
 
