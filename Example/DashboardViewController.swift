cfc385a4c61bd4dbf88054b38be16ae7f0a2d597
Cristian Madrid <cristianmadridd@gmail.com>
2017-09-27 19:47:02 -0300

    feat: add option on sample to test fullscreen required by app

---
+++ Example/DashboardViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+import Clappr
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var switchFullScreen: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func startVideo(_ sender: Any) {
+        performSegue(withIdentifier: "startVideo", sender: nil)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let viewController = segue.destination as? ViewController
+        let options: Options = [
+            kSourceUrl: "http://clappr.io/highline.mp4",
+            kPosterUrl: "http://clappr.io/poster.png",
+            kFullscreenByApp: switchFullScreen.isOn
+        ]
+        viewController?.options = options
+    }
+}
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -5,13 +5,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var playerContainer: UIView!
     var player: Player!
+    var options: Options = [:]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let options = [
-            kSourceUrl: "http://clappr.io/highline.mp4",
-            kPosterUrl: "http://clappr.io/poster.png",
-        ]
         player = Player(options: options)
 
         listenToPlayerEvents()
