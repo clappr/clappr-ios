490e322a9b81157d00313f4672fca3170dfd1fc5
Gabrielle Saman <gabi.duarte@gmail.com>
2017-10-04 18:47:10 -0300

    fix: block setFullscreen being called twice
    - Calls trigger from core
    - Refactor fullscreenStateHandler init

--- Clappr/Classes/Base/FullScreenStateHandler.swift
+++ Clappr/Classes/Base/FullScreenStateHandler.swift
@@ -1,6 +1,9 @@
 protocol FullscreenStateHandler {
 
-    init()
+    var core: Core? { get }
+    var isOnFullscreen: Bool { get }
+
+    init(core: Core)
 
     func enterInFullscreen(_: EventUserInfo)
     func enterInFullscreen()
@@ -18,53 +21,56 @@ extension FullscreenStateHandler {
     func exitFullscreen() {
         exitFullscreen([:])
     }
-}
 
-class FullscreenByApp: BaseObject, FullscreenStateHandler {
-
-    required override init() {
-        super.init()
-    }
-
-    func enterInFullscreen(_: EventUserInfo = [:]) {
-        trigger(Event.requestFullscreen.rawValue)
-    }
-
-    func exitFullscreen(_: EventUserInfo = [:]) {
-        trigger(Event.exitFullscreen.rawValue)
+    var isOnFullscreen: Bool {
+        return core?.isFullscreen ?? false
     }
 }
 
-class FullscreenByPlayer: BaseObject, FullscreenStateHandler {
+class FullscreenByApp: FullscreenStateHandler {
 
     weak var core: Core?
 
-    required override init() {
-        super.init()
-    }
-
-    convenience init(core: Core) {
-        self.init()
+    required init(core: Core) {
         self.core = core
     }
 
     func enterInFullscreen(_: EventUserInfo = [:]) {
-        guard let core = core else { return }
-        trigger(InternalEvent.willEnterFullscreen.rawValue)
+        guard !isOnFullscreen else { return }
+        core?.trigger(Event.requestFullscreen.rawValue)
+    }
+
+    func exitFullscreen(_: EventUserInfo = [:]) {
+        guard isOnFullscreen else { return }
+        core?.trigger(Event.exitFullscreen.rawValue)
+    }
+}
+
+class FullscreenByPlayer: FullscreenStateHandler {
+
+    weak var core: Core?
+
+    required init(core: Core) {
+        self.core = core
+    }
+
+    func enterInFullscreen(_: EventUserInfo = [:]) {
+        guard let core = core, !isOnFullscreen else { return }
+        core.trigger(InternalEvent.willEnterFullscreen.rawValue)
         core.mediaControl?.fullscreen = true
         core.fullscreenController.view.backgroundColor = UIColor.black
         core.fullscreenController.modalPresentationStyle = .overFullScreen
         core.parentController?.present(core.fullscreenController, animated: false, completion: nil)
         core.fullscreenController.view.addSubviewMatchingConstraints(core)
-        trigger(InternalEvent.didEnterFullscreen.rawValue)
+        core.trigger(InternalEvent.didEnterFullscreen.rawValue)
     }
 
     func exitFullscreen(_: EventUserInfo = [:]) {
-        guard let core = core else { return }
-        trigger(InternalEvent.willExitFullscreen.rawValue)
+        guard let core = core, isOnFullscreen else { return }
+        core.trigger(InternalEvent.willExitFullscreen.rawValue)
         core.mediaControl?.fullscreen = false
         core.parentView?.addSubviewMatchingConstraints(core)
         core.fullscreenController.dismiss(animated: false, completion: nil)
-        trigger(InternalEvent.didExitFullscreen.rawValue)
+        core.trigger(InternalEvent.didExitFullscreen.rawValue)
     }
 }
