5c2f7d17accfb46f1c51dfdb1bdd6bce5758923d
Cristian Madrid <cristianmadridd@gmail.com>
2017-10-23 17:34:07 -0200

    test: add identifier on container to do ui tests. Add test for start as fullscreen

--- Clappr_ExampleUITests/FullscreenUITest.swift
+++ Clappr_ExampleUITests/FullscreenUITest.swift
@@ -1,36 +1,30 @@
-//
-//  FullscreenUITest.swift
-//  Clappr
-//
-//  Created by Cristian Douglas dos Reis Madrid on 23/10/17.
-//  Copyright © 2017 CocoaPods. All rights reserved.
-//
-
 import XCTest
 
 class FullscreenUITest: XCTestCase {
-        
+
+    var dashboardInteractor: DashboardViewInteractor!
+    var app: XCUIApplication!
+
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
+        dashboardInteractor = DashboardViewInteractor(app: app)
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testStartAsFullscreen() {
+
+        dashboardInteractor.fullscreenControledByApp = false
+        dashboardInteractor.startAsFullscreen = true
+        dashboardInteractor.startVideo()
+
+        let container = app.otherElements["Container"]
+        let window = app.windows.element(boundBy: 0)
+        XCTAssert(container.frame == window.frame)
     }
     
 }
