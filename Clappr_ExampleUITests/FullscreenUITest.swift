c1ee9b99f2b1d2814a0f9bef74a5162a02a1f71f
Cristian Madrid <cristianmadridd@gmail.com>
2017-10-23 17:56:11 -0200

    test: add tests for fullscreenControledByApp add identifier on fullscreenbutton

--- Clappr_ExampleUITests/FullscreenUITest.swift
+++ Clappr_ExampleUITests/FullscreenUITest.swift
@@ -13,6 +13,10 @@ class FullscreenUITest: XCTestCase {
         return app.windows.element(boundBy: 0)
     }
 
+    var fullscreenButton: XCUIElement {
+        return app.buttons["FullscreenButton"]
+    }
+
     override func setUp() {
         super.setUp()
         app = XCUIApplication()
@@ -24,20 +28,39 @@ class FullscreenUITest: XCTestCase {
         super.tearDown()
     }
 
-    func testStartAsFullscreen() {
+    func testPlayerShouldHaveTheSameSizeOfWindow() {
         dashboardInteractor.fullscreenControledByApp = false
         dashboardInteractor.startAsFullscreen = true
         dashboardInteractor.startVideo()
         XCTAssert(container.frame == window.frame)
     }
 
-    func testStartAsFullscreenWithFullscreenControledByApp() {
+    func testPlayerShouldNotHaveTheSizeOfWindow() {
         dashboardInteractor.fullscreenControledByApp = true
         dashboardInteractor.startAsFullscreen = true
         dashboardInteractor.startVideo()
         XCTAssert(container.frame != window.frame)
     }
 
-    func 
-    
+    func testPlayerShouldNotChangeTheLayerSizeWhenStartAsFullscreenIsDisabled() {
+        dashboardInteractor.fullscreenControledByApp = true
+        dashboardInteractor.startAsFullscreen = false
+        dashboardInteractor.startVideo()
+
+        let currentFrame = container.frame
+        container.tap()
+        fullscreenButton.tap()
+        XCTAssert(currentFrame == container.frame)
+    }
+
+    func testPlayerShouldNotChangeTheLayerSizeWhenStartAsFullscreenIsEnabled() {
+        dashboardInteractor.fullscreenControledByApp = true
+        dashboardInteractor.startAsFullscreen = true
+        dashboardInteractor.startVideo()
+
+        let currentFrame = container.frame
+        container.tap()
+        fullscreenButton.tap()
+        XCTAssert(currentFrame == container.frame)
+    }
 }
