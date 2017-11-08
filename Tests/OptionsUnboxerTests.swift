a189b91c50d37da9ea2ce34bab0a5cafcb0d6564
Cristian Madrid <cristianmadridd@gmail.com>
2017-11-08 10:36:09 -0200

    test: create a class that contains OptionsUnboxerTests

---
+++ Tests/OptionsUnboxerTests.swift
@@ -0,0 +1,44 @@
+import Quick
+import Nimble
+@testable import Clappr
+
+class OptionsUnboxerTests: QuickSpec {
+    override func spec() {
+
+        describe(".OptionsUnboxer") {
+
+            var optionsUnboxer: OptionsUnboxer!
+
+            context("when no options is passed") {
+
+                beforeEach {
+                    optionsUnboxer = OptionsUnboxer(options: [:])
+                }
+
+                it("returns `false` for `fullscreen`") {
+                    expect(optionsUnboxer.fullscreen).to(beFalse())
+                }
+
+                it("returns `false` for `kFullscreenByApp`") {
+                    expect(optionsUnboxer.fullscreenControledByApp).to(beFalse())
+                }
+            }
+
+            context("when options is passed") {
+
+                beforeEach {
+                    optionsUnboxer = OptionsUnboxer(options: [kFullscreen: true, kFullscreenByApp: true])
+                }
+
+                it("should returns correct value for `fullscreen`") {
+                    expect(optionsUnboxer.fullscreen).to(beTrue())
+                }
+
+                it("should returns correct value for `kFullscreenByApp`") {
+                    expect(optionsUnboxer.fullscreenControledByApp).to(beTrue())
+                }
+            }
+        }
+    }
+}
+
