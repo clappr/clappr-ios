706e4f323ca172957d25d531da9949a531f09bc3
Juliana Chahoud <jchahoud@gmail.com>
2018-01-09 14:59:19 -0200

    refactor: move Clappr iOS Sample App to Example/Clappr folder

---
+++ org.cocoapods.demo.Clappr_tvOS_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  org.cocoapods.demo.Clappr_tvOS_Example
+//
+//  Created by conference on 09/01/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
