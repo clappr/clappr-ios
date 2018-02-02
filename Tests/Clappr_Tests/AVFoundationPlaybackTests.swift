64a5250254bf55c18cab8407d779c1d5c69e7f7d
Leandro Alonso <lalonso@thoughtworks.com>
2018-02-02 10:58:21 -0200

    test: adds tests for seek method

--- Tests/Clappr_Tests/AVFoundationPlaybackTests.swift
+++ Tests/Clappr_Tests/AVFoundationPlaybackTests.swift
@@ -1,5 +1,7 @@
 import Quick
 import Nimble
+import OHHTTPStubs
+
 @testable import Clappr
 
 class AVFoundationPlaybackTests: QuickSpec {
@@ -36,6 +38,59 @@ class AVFoundationPlaybackTests: QuickSpec {
                     let options = [kSourceUrl: "http://clappr.io/highline.zip"]
                     let canPlay = AVFoundationPlayback.canPlay(options as Options)
                     expect(canPlay) == false
+                }
+            }
+
+            describe("#seek") {
+
+                var avFoundationPlayback: AVFoundationPlayback!
+
+                beforeEach {
+                    stub(condition: isHost("clappr.io")) { _ in
+                        let stubPath = OHPathForFile("video.mp4", type(of: self))
+                        return fixture(filePath: stubPath!, headers: ["Content-Type":"video/mp4"])
+                    }
+                    avFoundationPlayback = AVFoundationPlayback(options: [kSourceUrl: "https://clappr.io/highline.mp4"])
+
+                    avFoundationPlayback.play()
+                }
+
+                it("triggers seek event") {
+                    waitUntil { done in
+                        let listener = BaseObject()
+
+                        listener.listenTo(avFoundationPlayback, eventName: Event.seek.rawValue) { info in
+                            done()
+                        }
+
+                        avFoundationPlayback.seek(5)
+                    }
+                }
+
+                it("triggers didSeek when a seek is completed") {
+                    waitUntil { done in
+                        let listener = BaseObject()
+
+                        listener.listenTo(avFoundationPlayback, eventName: Event.didSeek.rawValue) { info in
+                            expect(info!["success"] as? Bool).to(beTrue())
+                            done()
+                        }
+
+                        avFoundationPlayback.seek(5)
+                    }
+                }
+
+                it("triggers positionUpdate for the desired position") {
+                    waitUntil { done in
+                        let listener = BaseObject()
+
+                        listener.listenTo(avFoundationPlayback, eventName: Event.positionUpdate.rawValue) { info in
+                            expect(info!["position"] as? Float64).to(equal(5))
+                            done()
+                        }
+
+                        avFoundationPlayback.seek(5)
+                    }
                 }
             }
         }
