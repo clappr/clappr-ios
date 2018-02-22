ba8e68a7b8c45d3a3318831b55b00cd486c0c724
Cristian Madrid <cristianmadridd@gmail.com>
2018-02-22 19:39:58 -0300

    test: fix loadingtests

--- Tests/Clappr_UITests/Tests/LoadingCorePluginUITests.swift
+++ Tests/Clappr_UITests/Tests/LoadingCorePluginUITests.swift
@@ -7,30 +7,27 @@ class LoadingCorePluginUITests: BaseSpec {
     override func spec() {
         super.spec()
 
-        beforeEach {
-            XCUIDevice.shared.orientation = .portrait
-        }
-
         describe(".SpinnerPlugin") {
+
             it("shows the spinner when the player is initialized") {
                 self.dashboard.fullscreenControledByApp = false
+                self.dashboard.startAsFullscreen = false
 
                 self.dashboard.startVideo()
                 self.player.tapOnContainer()
-                self.player.tapOnFullscreen()
 
                 expect(self.app.otherElements["LoadingCorePlugin"].exists).toEventually(beTrue())
             }
 
             it("hides the spinner when the video starts") {
                 self.dashboard.fullscreenControledByApp = false
+                self.dashboard.startAsFullscreen = false
 
                 self.dashboard.startVideo()
                 self.player.tapOnContainer()
-                self.player.tapOnPlay()
 
                 self.waitVOD { done in
-                    expect(self.app.otherElements["LoadingCorePlugin"].exists).to(beFalse())
+                    expect(self.app.otherElements["LoadingCorePlugin"].exists).toEventually(beFalse())
                     done()
                 }
             }
