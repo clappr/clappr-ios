eac95c875d2063635ca192273de1923dcc86b525
Igor Oliveira <ioliveir@thoughtworks.com>
2018-03-05 11:42:19 -0300

    test: increase timeout to search for an element in UI

--- Tests/Clappr_UITests/XCTWaiter+WaitFor.swift
+++ Tests/Clappr_UITests/XCTWaiter+WaitFor.swift
@@ -3,7 +3,7 @@ import XCTest
 
 extension XCTWaiter {
 
-    func waitFor(element: XCUIElement, timeout: TimeInterval = 3) -> Bool {
+    func waitFor(element: XCUIElement, timeout: TimeInterval = 20) -> Bool {
         let existsPredicate = NSPredicate(format: "exists == true")
         let expectation = XCTNSPredicateExpectation(predicate: existsPredicate, object: element)
         let result = XCTWaiter().wait(for: [expectation], timeout: timeout)
