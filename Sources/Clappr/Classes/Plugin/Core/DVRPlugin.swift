a458c7a9257dfa42c264614e8dba3d757c775ec8
Nabil Safatli <safatli.nabil@gmail.com>
2018-06-20 19:08:08 -0300

    chore: moving DVRPlugin from Container to Core and making it trigger the dvr enabled following the rules of live and position > 100

---
+++ Sources/Clappr/Classes/Plugin/Core/DVRPlugin.swift
@@ -0,0 +1,47 @@
+open class DVRPlugin: UICorePlugin {
+    
+    open override var pluginName: String {
+        return "dvr"
+    }
+    
+    public required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public required init() {
+        super.init()
+    }
+    
+    @objc public required init(context: UIBaseObject) {
+        super.init(context: context)
+        bindEvents()
+    }
+}
+
+
+//MARK: Events Binding
+extension DVRPlugin {
+    fileprivate func bindEvents() {
+        stopListening()
+        
+        bindPlaybackEvents()
+        bindCoreEvents()
+        triggerDvrEvent()
+    }
+    
+    private func bindCoreEvents() {
+        guard let core = core else { return }
+        listenTo(core, eventName: InternalEvent.didChangeActivePlayback.rawValue) { [weak self] (_: EventUserInfo) in self?.bindEvents() }
+    }
+    
+    private func bindPlaybackEvents() {
+        guard let playback = self.core?.activeContainer?.playback else { return }
+        listenToOnce(playback, eventName: Event.bufferUpdate.rawValue) { [weak self] (_: EventUserInfo) in self?.triggerDvrEvent() }
+    }
+    
+    func triggerDvrEvent() {
+        guard let playback = self.core?.activeContainer?.playback as? AVFoundationPlayback else { return }
+        let dvrEnabled = playback.playbackType == .live && playback.position > 100
+        core?.trigger("enableDVR", userInfo: ["enabled": dvrEnabled])
+    }
+}
