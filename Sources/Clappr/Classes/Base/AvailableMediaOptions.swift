09801e3d2db201c393fe0d931cbcde2e9a99fa21
Rodrigo Dezouzart <rodrigodezouzart@gmail.com>
2018-09-19 17:01:58 -0300

    feat: make AvailableMediaOption's constructor public

--- Sources/Clappr/Classes/Base/AvailableMediaOptions.swift
+++ Sources/Clappr/Classes/Base/AvailableMediaOptions.swift
@@ -4,7 +4,7 @@ open class AvailableMediaOptions {
     open let sources: [MediaOption]
     open let hasDefaultSelected: Bool
 
-    init(_ sources: [MediaOption], hasDefaultSelected: Bool) {
+    public init(_ sources: [MediaOption], hasDefaultSelected: Bool) {
         self.sources = sources
         self.hasDefaultSelected = hasDefaultSelected
     }
