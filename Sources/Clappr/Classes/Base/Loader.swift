3a522b1fbd444f8725612e5dd983f3d2089e44f6
Nabil Safatli <safatli.nabil@gmail.com>
2018-09-27 15:20:04 -0300

    refactor: change Loader to a singleton and remove unnecessary MediaControl dependency

--- Sources/Clappr/Classes/Base/Loader.swift
+++ Sources/Clappr/Classes/Base/Loader.swift
@@ -1,54 +1,46 @@
 open class Loader {
-    internal(set) open var playbackPlugins: [Plugin.Type] = [AVFoundationPlayback.self, NoOpPlayback.self]
+    
+    static let shared = Loader()
+    
+    internal(set) open var playbacks: [Plugin.Type] = [AVFoundationPlayback.self, NoOpPlayback.self]
     #if os (iOS)
     internal(set) open var containerPlugins: [Plugin.Type] = [PosterPlugin.self, SpinnerPlugin.self]
     #else
     internal(set) open var containerPlugins: [Plugin.Type] = []
     #endif
     internal(set) open var corePlugins = [Plugin.Type]()
-
-    internal(set) open var mediaControl: MediaControl.Type = MediaControl.self
-
-    fileprivate var externalPlugins: [Plugin.Type]?
-
-    public init(options: Options = [:]) {
-        loadExternalMediaControl(options)
-
+    
+    private var externalPlugins: [Plugin.Type]?
+    
+    private init() {
         Logger.logInfo("plugins:" +
-            "\n - playback: \(playbackPlugins.map({ $0.name }))" +
+            "\n - playback: \(playbacks.map({ $0.name }))" +
             "\n - container: \(containerPlugins.map({ $0.name }))" +
-            "\n - core: \(corePlugins.map({ $0.name }))" +
-            "\n - mediaControl: \(mediaControl)", scope: "\(type(of: self))")
+            "\n - core: \(corePlugins.map({ $0.name }))")
     }
-
-    fileprivate func loadExternalMediaControl(_ options: Options) {
-        if let externalMediaControl = options[kMediaControl] as? MediaControl.Type {
-            mediaControl = externalMediaControl
-        }
-    }
-
+    
     open func addExternalPlugins(_ externalPlugins: [Plugin.Type]) {
         self.externalPlugins = externalPlugins
-        playbackPlugins = getPlugins(.playback, defaultPlugins: playbackPlugins)
+        playbacks = getPlugins(.playback, defaultPlugins: playbacks)
         containerPlugins = getPlugins(.container, defaultPlugins: containerPlugins)
         corePlugins = getPlugins(.core, defaultPlugins: corePlugins)
     }
-
-    fileprivate func getPlugins(_ type: PluginType, defaultPlugins: [Plugin.Type]) -> [Plugin.Type] {
+    
+    private func getPlugins(_ type: PluginType, defaultPlugins: [Plugin.Type]) -> [Plugin.Type] {
         guard let externalPlugins = externalPlugins else { return [] }
         let filteredPlugins = externalPlugins.filter({ $0.type == type })
         return removeDuplicate(filteredPlugins + defaultPlugins)
     }
-
-    fileprivate func removeDuplicate(_ plugins: [Plugin.Type]) -> [Plugin.Type] {
+    
+    private func removeDuplicate(_ plugins: [Plugin.Type]) -> [Plugin.Type] {
         var result = [Plugin.Type]()
-
+        
         for plugin in plugins {
             if !result.contains(where: { $0.name == plugin.name }) {
                 result.append(plugin)
             }
         }
-
+        
         return result
     }
 }
