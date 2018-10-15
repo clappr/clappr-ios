6f39d3fd9024986c98166a9bc85d2a49dd2cad4f
Bruno Lins <brunovpl@gmail.com>
2018-10-15 16:31:53 -0300

    test: Add TimeIndicator tests

---
+++ Tests/Clappr_Tests/Classes/Plugins/Core/MediaControl/TimeIndicatorTests.swift
@@ -0,0 +1,193 @@
+import Quick
+import Nimble
+
+@testable import Clappr
+
+class TimeIndicatorTests: QuickSpec {
+    override func spec() {
+        describe(".TimeIndicatorTests") {
+            it("is a MediaControlPlugin") {
+                let timeIndicator = TimeIndicator()
+
+                expect(timeIndicator).to(beAKindOf(MediaControlPlugin.self))
+            }
+
+            describe("#pluginName") {
+                it("is TimeIndicator") {
+                    let timeIndicator = TimeIndicator()
+
+                    expect(timeIndicator.pluginName).to(equal("TimeIndicator"))
+                }
+            }
+
+            describe("#panel") {
+                it("is positioned in the bottom panel") {
+                    let timeIndicator = TimeIndicator()
+
+                    expect(timeIndicator.panel).to(equal(MediaControlPanel.bottom))
+                }
+            }
+
+            describe("#position") {
+                it("is positioned in the left side") {
+                    let timeIndicator = TimeIndicator()
+
+                    expect(timeIndicator.position).to(equal(MediaControlPosition.left))
+                }
+            }
+
+            describe("#render") {
+
+                var timeIndicator: TimeIndicator!
+
+                beforeEach {
+                    timeIndicator = TimeIndicator()
+
+                    timeIndicator.render()
+                }
+
+                describe("time indicator") {
+                    it("creates it as a UIStackView") {
+                        expect(timeIndicator.indicator).to(beAKindOf(UIStackView.self))
+                    }
+
+                    it("sets the acessibilityIdentifier") {
+                        expect(timeIndicator.indicator.accessibilityIdentifier).to(equal("timeIndicator"))
+                    }
+                }
+
+                describe("elapsedTime") {
+                    it("is a UILabel") {
+                        expect(timeIndicator.elapsedTimeLabel).to(beAKindOf(UILabel.self))
+                    }
+
+                    it("has white color") {
+                        expect(timeIndicator.elapsedTimeLabel.textColor).to(equal(UIColor.white))
+                    }
+
+                    it("has '00:00' as initial text") {
+                        expect(timeIndicator.elapsedTimeLabel.text).to(equal("00:00"))
+                    }
+
+                    it("has Bold size 14") {
+                        expect(timeIndicator.elapsedTimeLabel.font).to(equal(UIFont.boldSystemFont(ofSize:  14)))
+                    }
+                }
+
+                describe("separator") {
+                    it("is a UILabel") {
+                        expect(timeIndicator.separatorLabel).to(beAKindOf(UILabel.self))
+                    }
+
+                    it("has white color") {
+                        expect(timeIndicator.separatorLabel.textColor).to(equal(UIColor.white))
+                    }
+
+                    it("has ' / ' as initial text") {
+                        expect(timeIndicator.separatorLabel.text).to(equal(" / "))
+                    }
+
+                    it("has OpenSans Bold size 14") {
+                        expect(timeIndicator.separatorLabel.font).to(equal(UIFont.boldSystemFont(ofSize: 14)))
+                    }
+                }
+
+                describe("durationTime") {
+                    it("is a UILabel") {
+                        expect(timeIndicator.durationTimeLabel).to(beAKindOf(UILabel.self))
+                    }
+
+                    it("has white color") {
+                        expect(timeIndicator.durationTimeLabel.textColor).to(equal(UIColor.white))
+                    }
+
+                    it("has '00:00' as initial text") {
+                        expect(timeIndicator.durationTimeLabel.text).to(equal("00:00"))
+                    }
+
+                    it("has OpenSans Bold size 14") {
+                        expect(timeIndicator.durationTimeLabel.font).to(equal(UIFont.boldSystemFont(ofSize: 14)))
+                    }
+                }
+
+
+                describe("leftMargin") {
+                    it("is a UIView") {
+                        expect(timeIndicator.leftMargin).to(beAKindOf(UIView.self))
+                    }
+
+                    it("is the first view in the time indicator") {
+                        expect(timeIndicator.indicator.subviews.first).to(equal(timeIndicator.leftMargin))
+                    }
+                }
+            }
+
+            describe("when player is ready") {
+                var coreStub: CoreStub!
+                var timeIndicator: TimeIndicator!
+
+                beforeEach {
+                    coreStub = CoreStub()
+                    timeIndicator = TimeIndicator(context: coreStub)
+
+                    timeIndicator.render()
+                }
+
+                it("updates the duration time of the video label") {
+                    coreStub.playbackMock?.videoDuration = 138.505
+
+                    coreStub.activePlayback?.trigger(Event.ready)
+
+                    expect(timeIndicator.durationTimeLabel.text).to(equal("02:18"))
+                }
+            }
+
+            describe("when a position update event is triggered") {
+                it("updates the elapsed time label") {
+                    let coreStub = CoreStub()
+                    let timeIndicator = TimeIndicator(context: coreStub)
+                    timeIndicator.render()
+                    let userInfo: EventUserInfo = ["position": TimeInterval(50)]
+
+                    coreStub.activePlayback?.trigger(Event.positionUpdate, userInfo: userInfo)
+
+                    expect(timeIndicator.elapsedTimeLabel.text).to(equal("00:50"))
+                }
+            }
+
+            describe("Fullscreen") {
+                var coreStub: CoreStub!
+                var timeIndicator: TimeIndicator!
+
+                beforeEach {
+                    coreStub = CoreStub()
+                    timeIndicator = TimeIndicator(context: coreStub)
+
+                    timeIndicator.render()
+                }
+
+                context("when user enters in fullscreen") {
+                    it("update layout constants for medium screen") {
+                        coreStub.activeContainer?.frame = CGRect(x: 0, y: 0, width: 500, height: 0)
+
+                        coreStub.trigger(InternalEvent.didEnterFullscreen.rawValue)
+
+                        expect(timeIndicator.leftMarginSize?.constant).to(equal(16))
+                        expect(timeIndicator.marginBottom).to(equal(10))
+                    }
+                }
+
+                context("when user leaves fullscreen") {
+                    it("update layout constants for small screen") {
+                        coreStub.activeContainer?.frame = CGRect(x: 0, y: 0, width: 200, height: 0)
+
+                        coreStub.trigger(InternalEvent.didExitFullscreen.rawValue)
+
+                        expect(timeIndicator.leftMarginSize?.constant).to(equal(16))
+                        expect(timeIndicator.marginBottom).to(equal(10))
+                    }
+                }
+            }
+        }
+    }
+}
