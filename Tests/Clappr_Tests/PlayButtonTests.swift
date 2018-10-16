e3d585fe97c21d92335dff08e61fb035b664b8e6
Bruno Lins <brunovpl@gmail.com>
2018-10-16 10:18:21 -0300

    test: Move UIImage extension to its own file

--- Tests/Clappr_Tests/PlayButtonTests.swift
+++ Tests/Clappr_Tests/PlayButtonTests.swift
@@ -232,13 +232,3 @@ class PlayButtonTests: QuickSpec {
         }
     }
 }
-
-extension UIImage {
-
-    func isEqualTo(image: UIImage) -> Bool {
-        let data1: Data = UIImagePNGRepresentation(self)!
-        let data2: Data = UIImagePNGRepresentation(image)!
-        return data1 == data2
-    }
-
-}
