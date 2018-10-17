a0324e2b23258c656d1c409874a529c3e7283cab
Bruno Lins <brunovpl@gmail.com>
2018-10-17 14:45:06 -0300

    test: Removing unecessart UIImage extension to define equals method and refactor the code according it

--- Tests/Clappr_Tests/Classes/Extensions/UIImageExtension.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UIImage {
-    func isEqualTo(image: UIImage) -> Bool {
-        let data1: Data = UIImagePNGRepresentation(self)!
-        let data2: Data = UIImagePNGRepresentation(image)!
-        return data1 == data2
-    }
-}
