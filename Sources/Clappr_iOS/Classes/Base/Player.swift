71573d0ced9a5f0ab9adbe1886c0a7074e4806d8
Leandro Alonso <lalonso@thoughtworks.com>
2018-10-24 17:06:12 -0300

    feat: add MediaControl to the list of built-in plugins

--- Sources/Clappr_iOS/Classes/Base/Player.swift
+++ Sources/Clappr_iOS/Classes/Base/Player.swift
@@ -162,7 +162,7 @@ open class Player: BaseObject {
 
         playbackEventsListenIds.removeAll()
     }
-    
+
     open class func register(plugins: [Plugin.Type]) {
         if !hasAlreadyRegisteredPlugins {
             let builtInPlugins: [Plugin.Type] = [AVFoundationPlayback.self, MediaControl.self, PosterPlugin.self, SpinnerPlugin.self, PlayButton.self, TimeIndicator.self, FullscreenButton.self]
