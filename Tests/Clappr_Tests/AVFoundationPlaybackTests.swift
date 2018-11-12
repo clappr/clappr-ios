43d3f0ac5d069ba174ba47681a3165bbbb9e3e83
Leandro Alonso <lalonso@thoughtworks.com>
2018-11-12 15:23:24 -0200

    fix: stop the playback to avoid multiple instances

--- Tests/Clappr_Tests/AVFoundationPlaybackTests.swift
+++ Tests/Clappr_Tests/AVFoundationPlaybackTests.swift
@@ -38,6 +38,10 @@ class AVFoundationPlaybackTests: QuickSpec {
                 playback = AVFoundationPlayback()
                 playback.player = player
             }
+            
+            afterEach {
+                playback.stop()
+            }
 
             describe("AVFoundationPlaybackExtension") {
                 describe("#minDvrSize") {
