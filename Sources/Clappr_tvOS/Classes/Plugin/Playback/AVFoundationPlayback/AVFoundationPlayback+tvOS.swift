2b9bfb5e90c768718b305e656c0287d1de273a40
Geison Goes <ggoes@thoughtworks.com>
2018-12-10 14:34:28 -0200

    fix: tvOS warnings

--- Sources/Clappr_tvOS/Classes/Plugin/Playback/AVFoundationPlayback/AVFoundationPlayback+tvOS.swift
+++ Sources/Clappr_tvOS/Classes/Plugin/Playback/AVFoundationPlayback/AVFoundationPlayback+tvOS.swift
@@ -14,12 +14,10 @@ extension AVFoundationPlayback: AVPlayerViewControllerDelegate {
     public func playerViewController(_ playerViewController: AVPlayerViewController, didSelect mediaSelectionOption: AVMediaSelectionOption?, in mediaSelectionGroup: AVMediaSelectionGroup) {
         guard let mediaType = mediaSelectionGroup.options.first?.mediaType else { return }
         if mediaType == AVMediaType.subtitle.rawValue {
-            triggerMediaOptionSelectedEvent(option: selectedSubtitle, event: Event.subtitleSelected)
             triggerMediaOptionSelectedEvent(option: selectedSubtitle, event: Event.didSelectSubtitle)
         }
 
         if mediaType == AVMediaType.audio.rawValue {
-            triggerMediaOptionSelectedEvent(option: selectedAudioSource, event: Event.audioSelected)
             triggerMediaOptionSelectedEvent(option: selectedAudioSource, event: Event.didSelectAudio)
         }
     }
