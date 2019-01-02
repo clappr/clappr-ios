a2ecf4341627c046d49cb8d9637662683b93a992
Geison Goes <ggoes@thoughtworks.com>
2019-01-02 16:19:25 -0200

    refactor: extract bitrate information from AvFoundation to a extension in a separate file

--- Sources/Clappr/Classes/Plugin/Playback/AVFoundationPlayback.swift
+++ Sources/Clappr/Classes/Plugin/Playback/AVFoundationPlayback.swift
@@ -697,26 +697,3 @@ extension AVFoundationPlayback {
         return 5
     }
 }
-
-// MARK: - Metrics
-extension AVFoundationPlayback {
-    public func getBitrate() -> Double? {
-        guard let logEvent = lastLogEvent() else { return nil }
-        if (logEvent.segmentsDownloadedDuration ) > 0 {
-            return logEvent.indicatedBitrate
-        }
-        return logEvent.observedBitrate
-    }
-
-    public func getAvgBitrate() -> Double? {
-        guard let logEvent = lastLogEvent() else { return nil }
-        if #available(iOS 10.0, *) {
-            return logEvent.averageVideoBitrate
-        }
-        return nil
-    }
-
-    private func lastLogEvent() -> AVPlayerItemAccessLogEvent? {
-        return player?.currentItem?.accessLog()?.events.last
-    }
-}
