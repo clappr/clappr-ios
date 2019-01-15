e55f3c41713d14683776a3575de0a55185b806a2
Leandro Alonso <lalonso@thoughtworks.com>
2019-01-15 13:09:43 -0200

    feat: convert NSExceptions to Swift Esceptions

--- Sources/Clappr.h
+++ Sources/Clappr.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ClapprVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Clappr/PublicHeader.h>
 
-
+#import "ObjC.h"
