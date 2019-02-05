092ea1b3bdca49bce51962de74931e28d4de4f36
Carlos Corrêa <caiobzen@gmail.com>
2019-02-05 09:34:57 -0200

    feat: listening to modal events to enable or disable touch

--- Sources/Clappr/Classes/Plugin/Core/DoubleTapPlugin.swift
+++ Sources/Clappr/Classes/Plugin/Core/DoubleTapPlugin.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class DoubleTapPlugin: UICorePlugin {
+public class DoubleTapPlugin: UICorePlugin {
     
     override open var pluginName: String {
         return String(describing: DoubleTapPlugin.self)
@@ -21,28 +21,52 @@ class DoubleTapPlugin: UICorePlugin {
     required init(context: UIObject) {
         super.init(context: context)
         animatonHandler = DoubleTapAnimation(core)
+        bindEvents()
     }
     
     required init() {
         super.init()
     }
     
-    override func render() {
+    private func bindEvents() {
+        stopListening()
+        bindCoreEvents()
+    }
+    
+    private func bindCoreEvents() {
+        guard let core = core else { return }
+        listenTo(core, eventName: Event.didChangeActiveContainer.rawValue) { [weak self] _ in self?.bindEvents() }
+        listenTo(core, eventName: Event.didShowModal.rawValue) { [weak self] _ in self?.didShowModal() }
+        listenTo(core, eventName: Event.didHideModal.rawValue) { [weak self] _ in self?.didHideModal() }
+    }
+    
+    private func didShowModal() {
+        doubleTapView.isUserInteractionEnabled = false
+    }
+    
+    private func didHideModal() {
+        doubleTapView.isUserInteractionEnabled = true
+    }
+    
+    override public func render() {
+        doubleTapView.core = core
+        doubleTapView.backgroundColor = .clear
+        core?.view.addSubview(doubleTapView)
+        doubleTapView.bindFrameToSuperviewBounds()
+        addGestures()
+    }
+    
+    private func addGestures() {
         let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(doubleTap))
         doubleTapGesture.numberOfTapsRequired = 2
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(singleTap))
         tapGesture.require(toFail: doubleTapGesture)
         
-        doubleTapView.core = core
-        doubleTapView.backgroundColor = .clear
-        core?.view.addSubview(doubleTapView)
-        doubleTapView.bindFrameToSuperviewBounds()
-        
         doubleTapView.addGestureRecognizer(doubleTapGesture)
         doubleTapView.addGestureRecognizer(tapGesture)
     }
-    
+
     @objc private func doubleTap(gestureRecognizer: UITapGestureRecognizer) {
         if gestureRecognizer.state == .recognized {
             let xPosition = gestureRecognizer.location(in: view).x
@@ -103,12 +127,19 @@ class PassthroughView: UIView {
     private func mediaControlPluginsColidesWithTouch(point: CGPoint, event: UIEvent?) -> Bool {
         guard let mediaControlPlugin = core?.plugins.first(where: { $0 is MediaControl }) else { return false }
         
-        let pluginsWithoutMediaControl = core?.plugins.filter({ $0 is MediaControlPlugin })
-        let pluginColidingWithGesture = pluginsWithoutMediaControl?.first(where: { pluginColideWithTouch($0, point: point, event: event) })
+        let pluginColidingWithGesture = filteredOutModalPlugins()?.first(where: {
+            pluginColideWithTouch($0, point: point, event: event)
+        })
+        
         return pluginColidingWithGesture == nil || mediaControlPlugin.view.isHidden
     }
     
+    private func filteredOutModalPlugins() -> [UICorePlugin]? {
+        let pluginsWithoutMediaControl = core?.plugins.filter({ $0.pluginName != MediaControl.name })
+        return pluginsWithoutMediaControl?.filter({ ($0 as? MediaControlPlugin)?.panel != .modal })
+    }
+    
     private func pluginColideWithTouch(_ plugin: UICorePlugin, point: CGPoint, event: UIEvent?) -> Bool {
-         return plugin.view.point(inside: self.convert(point, to: plugin.view), with: event)
+        return plugin.view.point(inside: self.convert(point, to: plugin.view), with: event)
     }
 }
