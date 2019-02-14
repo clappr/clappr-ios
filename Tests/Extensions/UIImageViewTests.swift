6cdddd1f69dc0b21f03f8fa649cc51fdc42ae746
Paulo Gama <pgama@thoughtworks.com>
2019-02-14 14:43:35 -0200

    test: adjusting test to avoid cache

--- Tests/Extensions/UIImageViewTests.swift
+++ Tests/Extensions/UIImageViewTests.swift
@@ -14,7 +14,7 @@ class UIImageViewTests: QuickSpec {
 
                 context("when the response is 200") {
                     beforeEach {
-                        stub(condition: isExtension("png") && isHost("clappr.io")) { _ in
+                        stub(condition: isExtension("png") && isHost("test200")) { _ in
                             let image = UIImage(named: "poster", in: Bundle(for: UIImageViewTests.self), compatibleWith: nil)!
                             let data = UIImagePNGRepresentation(image)
                             return OHHTTPStubsResponse(data: data!, statusCode: 200, headers: ["Content-Type":"image/jpeg"])
@@ -22,7 +22,7 @@ class UIImageViewTests: QuickSpec {
                     }
 
                     it("sets image") {
-                        let url = URL(string: "https://clappr.io/poster.png")
+                        let url = URL(string: "https://test200/poster.png")
                         let imageView = UIImageView()
 
                         imageView.setImage(from: url!)
@@ -33,13 +33,13 @@ class UIImageViewTests: QuickSpec {
 
                 context("when the response is different of 200") {
                     beforeEach {
-                        stub(condition: isExtension("png") && isHost("clappr.io")) { _ in
+                        stub(condition: isExtension("png") && isHost("test400")) { _ in
                             return OHHTTPStubsResponse(data: Data(), statusCode: 400, headers: [:])
                         }
                     }
 
                     it("doesn't set image") {
-                        let url = URL(string: "https://clappr.io/poster.png")
+                        let url = URL(string: "https://test400/poster.png")
                         let imageView = UIImageView()
 
                         imageView.setImage(from: url!)
