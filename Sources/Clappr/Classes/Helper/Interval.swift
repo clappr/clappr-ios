7cf60c16084a1e6db0dea26890b81d63cfdd6dac
Carlos Corrêa <caiobzen@gmail.com>
2019-02-14 10:01:47 -0200

    feat: add intervals class and use constants where animation happens

---
+++ Sources/Clappr/Classes/Helper/Interval.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class Interval {
+    
+    static let seekBubbleShowAnimation: TimeInterval = 4.0
+    static let seekBubbleDismissAnimation: TimeInterval = 2.0
+    static let seekBubbleVisibility: TimeInterval =  0.3
+    
+    static let seekLabelShowAnimation: TimeInterval = 2.0
+    static let seekLabelHideAnimation: TimeInterval = 2.0
+    static let seekLabelVisibility: TimeInterval =  0.5
+    
+    static let seekImageAnimation: TimeInterval = 0.15
+    static let seekImageVisibility: TimeInterval = 0.15
+    
+    static let mediaControlAnimation: TimeInterval = 0.3
+    static let mediaControlSlowVisibility: TimeInterval = 4.0
+    static let mediaControlVisibility: TimeInterval = 0.4
+}
