02a08e2a1de5bb0bc15abc06818d13f3eed7f90f
Carlos Corrêa <caiobzen@gmail.com>
2019-02-25 11:45:52 -0300

    refactor: remove view from UIPlugin

--- Sources/Clappr/Classes/Plugin/Container/UIContainerPlugin.swift
+++ Sources/Clappr/Classes/Plugin/Container/UIContainerPlugin.swift
@@ -1,6 +1,9 @@
 open class UIContainerPlugin: ContainerPlugin, UIPlugin {
     var uiObject = UIObject()
-    public var view: UIView = UIView()
+    
+    public var view: UIView {
+        return uiObject.view
+    }
     
     open func render() {
         uiObject.render()
--- Sources/Clappr/Classes/Plugin/UIPlugin.swift
+++ Sources/Clappr/Classes/Plugin/UIPlugin.swift
@@ -1,15 +1,10 @@
 protocol UIPlugin: Plugin {
     var uiObject: UIObject { get }
-    var view: UIView { get set }
     func render()
 }
 
 extension UIPlugin {
     func render() {
         uiObject.render()
-    }
-    
-    var view: UIView {
-        return uiObject.view
     }
 }
