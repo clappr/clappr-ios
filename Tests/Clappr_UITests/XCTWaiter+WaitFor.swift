0f2a3582d986d7fba745d4018dc4c140d9e1aced
Ricardo Cavalcanti <rcaval@thoughtworks.com>
2019-05-21 16:38:45 -0300

    refact: make extension a true instance extension

--- Tests/Clappr_UITests/XCTWaiter+WaitFor.swift
+++ Tests/Clappr_UITests/XCTWaiter+WaitFor.swift
@@ -6,7 +6,7 @@ extension XCTWaiter {
     func waitFor(element: XCUIElement, timeout: TimeInterval = 20) -> Bool {
         let existsPredicate = NSPredicate(format: "exists == true")
         let expectation = XCTNSPredicateExpectation(predicate: existsPredicate, object: element)
-        let result = XCTWaiter().wait(for: [expectation], timeout: timeout)
+        let result = self.wait(for: [expectation], timeout: timeout)
         return result == .completed
     }
 }
