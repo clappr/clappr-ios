ee0e28147f9e97f530a9b342dc1fff333ecaf195
Paulo Gama <pgama@thoughtworks.com>
2019-05-28 19:55:53 -0300

    refactor: change empty return to void

--- Sources/Clappr/Classes/Extension/AVAsset+Ext.swift
+++ Sources/Clappr/Classes/Extension/AVAsset+Ext.swift
@@ -1,7 +1,7 @@
 import AVKit
 
 public extension AVAsset {
-    func wait(for property: String, then completion: @escaping () -> ()) {
+    func wait(for property: String, then completion: @escaping () -> Void) {
         self.loadValuesAsynchronously(forKeys: [property]) {
             DispatchQueue.main.async {
                 completion()
