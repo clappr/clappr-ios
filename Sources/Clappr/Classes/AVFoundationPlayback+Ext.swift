833a21c699a8ef8bf8d8ea84af6643360a2b3e6c
Ricardo Cavalcanti <rcaval@thoughtworks.com>
2019-06-06 18:03:26 -0300

    feat: adds wait for based on the playback keypaths

--- Sources/Clappr/Classes/AVFoundationPlayback+Ext.swift
+++ Sources/Clappr/Classes/AVFoundationPlayback+Ext.swift
@@ -27,4 +27,25 @@ public extension Playback {
             completion()
         }
     }
+
+    func wait(for keypath: PartialKeyPath<Playback>, then completion: @escaping () -> Void) {
+        if let playback = self as? AVFoundationPlayback, let property = Playback.waitKeypaths[keypath] {
+            playback.player?.currentItem?.asset.wait(for: property, then: completion)
+        } else {
+            completion()
+        }
+    }
+
+    private static let  waitKeypaths: [PartialKeyPath<Playback>: AVAssetProperty]  = [
+            \Playback.playbackType: AVAssetProperty.duration,
+            \Playback.duration: AVAssetProperty.duration,
+            \Playback.position: AVAssetProperty.duration,
+            \Playback.isDvrAvailable: AVAssetProperty.duration,
+            \Playback.isDvrInUse: AVAssetProperty.duration,
+            \Playback.canPause: AVAssetProperty.duration,
+            \Playback.audioSources: AVAssetProperty.characteristics,
+            \Playback.subtitles: AVAssetProperty.characteristics,
+            \Playback.selectedAudioSource: AVAssetProperty.characteristics,
+            \Playback.selectedSubtitle: AVAssetProperty.characteristics,
+        ]
 }
