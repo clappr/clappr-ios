55dda332bee3691685167ab86dd1a99d90d1044c
Carlos Corrêa <caiobzen@gmail.com>
2019-08-22 10:30:56 -0300

    test: refactor tests to reuse some variables

--- Tests/Clappr_Tests/Classes/Plugins/Core/OverlayPluginTests.swift
+++ Tests/Clappr_Tests/Classes/Plugins/Core/OverlayPluginTests.swift
@@ -6,24 +6,23 @@ class OverlayPluginTests: QuickSpec {
     override func spec() {
         describe("OverlayPlugin") {
             describe("#init") {
-                it("has a name") {
-                    let core = CoreStub()
-                    let plugin = OverlayPlugin(context: core)
+                var core: CoreStub!
+                var plugin: OverlayPlugin!
 
+                beforeEach {
+                    core = CoreStub()
+                    plugin = OverlayPlugin(context: core)
+                }
+
+                it("has a name") {
                     expect(plugin.pluginName).to(equal("OverlayPlugin"))
                 }
 
                 it("is a kind of UICorePlugin") {
-                    let core = CoreStub()
-                    let plugin = OverlayPlugin(context: core)
-
                     expect(plugin).to(beAKindOf(UICorePlugin.self))
                 }
 
                 it("starts with modal flag set to false") {
-                    let core = CoreStub()
-                    let plugin = OverlayPlugin(context: core)
-
                     expect(plugin.isModal).to(beFalse())
                 }
             }
