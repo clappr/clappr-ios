2da04db6a75a46e84fc92071b853dee485f86529
Paulo Gama <pgama@thoughtworks.com>
2019-09-23 09:12:10 -0300

    refactor: change alpha name to be more descriptive

--- Sources/Clappr_iOS/Classes/Plugin/Core/Drawer/BottomDrawerPlugin.swift
+++ Sources/Clappr_iOS/Classes/Plugin/Core/Drawer/BottomDrawerPlugin.swift
@@ -98,12 +98,12 @@ open class BottomDrawerPlugin: DrawerPlugin {
 
         if canDrag(with: gesture.newYCoordinate) {
             let portionShown = initialCenterY - gesture.newYCoordinate
-            let alpha = hiddenHeight / portionShown * 0.08
+            let mediaControlAlpha = hiddenHeight / portionShown * 0.08
 
             view.center.y = gesture.newYCoordinate
             view.alpha = 1
 
-            core?.trigger(InternalEvent.didDragDrawer.rawValue, userInfo: ["alpha": alpha])
+            core?.trigger(InternalEvent.didDragDrawer.rawValue, userInfo: ["alpha": mediaControlAlpha])
         }
     }
 
