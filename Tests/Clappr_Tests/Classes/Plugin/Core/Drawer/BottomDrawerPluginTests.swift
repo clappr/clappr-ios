ee0d5deab902416e3ed79796d3f5b0b46753f633
John <john.kmartins@gmail.com>
2020-01-20 17:15:59 -0300

    fix: correct the broken unit tests

--- Tests/Clappr_Tests/Classes/Plugin/Core/Drawer/BottomDrawerPluginTests.swift
+++ Tests/Clappr_Tests/Classes/Plugin/Core/Drawer/BottomDrawerPluginTests.swift
@@ -7,6 +7,10 @@ class BottomDrawerPluginTests: QuickSpec {
 
     override func spec() {
         describe(".BottomDrawerPlugin") {
+
+            let coreViewHeight = CGFloat(100)
+            let coreViewWidth = CGFloat(320)
+
             context("#init") {
                 var core: CoreStub!
                 var plugin: BottomDrawerPlugin!
@@ -21,9 +25,9 @@ class BottomDrawerPluginTests: QuickSpec {
                 }
 
                 it("has size with the same width and the half of the height from parentView") {
-                    core.view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
+                    core.view = UIView(frame: CGRect(x: 0, y: 0, width: coreViewWidth, height: coreViewHeight))
 
-                    expect(plugin.size).to(equal(CGSize(width: 320, height: 50)))
+                    expect(plugin.size).to(equal(CGSize(width: coreViewWidth, height: coreViewHeight/2)))
                 }
             }
 
@@ -34,29 +38,29 @@ class BottomDrawerPluginTests: QuickSpec {
                 beforeEach {
                     core = CoreStub()
                     plugin = BottomDrawerPlugin(context: core)
-                    core.view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
+                    core.view = UIView(frame: CGRect(x: 0, y: 0, width: coreViewWidth, height: coreViewHeight))
                 }
 
                 it("has origin x on zero and y equal to parentView height") {
                     plugin.render()
 
-                    expect(plugin.view.frame.origin).to(equal(CGPoint(x: .zero, y: 100)))
+                    expect(plugin.view.frame.origin).to(equal(CGPoint(x: .zero, y: coreViewHeight)))
                 }
 
                 it("has a view frame size equals to his size") {
                     plugin.render()
 
-                    expect(plugin.view.frame.size).to(equal(CGSize(width: 320, height: 50)))
+                    expect(plugin.view.frame.size).to(equal(CGSize(width: coreViewWidth, height: coreViewHeight/2)))
                 }
 
                 context("when plugin request a height greater then the limit") {
                     it("uses the height limit instead") {
-                        core.view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
+                        core.view = UIView(frame: CGRect(x: 0, y: 0, width: coreViewWidth, height: coreViewHeight))
                         let pluginMock = BottomDrawerPluginMock(context: core)
 
                         pluginMock.render()
 
-                        expect(pluginMock.view.frame.size).to(equal(CGSize(width: 320, height: 50)))
+                        expect(pluginMock.view.frame.size).to(equal(CGSize(width: coreViewWidth, height: coreViewHeight/2)))
                     }
                 }
             }
@@ -66,9 +70,10 @@ class BottomDrawerPluginTests: QuickSpec {
                 var plugin: BottomDrawerPlugin!
 
                 beforeEach {
+                    Loader.shared.resetPlugins()
                     core = CoreStub()
                     plugin = BottomDrawerPlugin(context: core)
-                    core.view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
+                    core.view = UIView(frame: CGRect(x: 0, y: 0, width: coreViewWidth, height: coreViewHeight))
                     plugin.render()
                 }
 
@@ -76,7 +81,7 @@ class BottomDrawerPluginTests: QuickSpec {
                     it("push up the drawer to half of core height") {
                         core.trigger(.showDrawerPlugin)
 
-                        expect(plugin.view.frame.origin).to(equal(CGPoint(x: .zero, y: 50)))
+                        expect(plugin.view.frame.origin.y).to(equal(coreViewHeight/2))
                     }
                 }
 
@@ -84,7 +89,7 @@ class BottomDrawerPluginTests: QuickSpec {
                     it("push down the drawer to core height") {
                         core.trigger(.hideDrawerPlugin)
 
-                        expect(plugin.view.frame.origin).to(equal(CGPoint(x: .zero, y: 100)))
+                        expect(plugin.view.frame.origin.y).to(equal(coreViewHeight))
                     }
                 }
             }
