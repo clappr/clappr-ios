29342d73974dfbd449de6ce00b600f8f60662fae
John <john.kmartins@gmail.com>
2020-02-28 19:34:23 -0300

    refact: remove extension

--- Sources/Clappr/Classes/Plugin/Playback/AVFoundationPlayback.swift
+++ Sources/Clappr/Classes/Plugin/Playback/AVFoundationPlayback.swift
@@ -31,6 +31,15 @@ open class AVFoundationPlayback: Playback {
 
     private var observers = [NSKeyValueObservation]()
 
+    private var lastLogEvent: AVPlayerItemAccessLogEvent? { player?.currentItem?.accessLog()?.events.last }
+
+    open var bitrate: Double? { lastLogEvent?.indicatedBitrate }
+    open var bandwidth: Double? { lastLogEvent?.observedBitrate }
+    open var averageBitrate: Double? { lastLogEvent?.averageVideoBitrate }
+    open var droppedFrames: Int? { lastLogEvent?.numberOfDroppedVideoFrames }
+    open var totalFrames: Int? { 0 }
+    open var domainHost: String? { asset?.url.host }
+
     var lastDvrAvailability: Bool?
     #if os(tvOS)
     lazy var nowPlayingService: AVFoundationNowPlayingService = {
