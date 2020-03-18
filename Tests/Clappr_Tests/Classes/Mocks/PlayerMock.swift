979daa45fe0766ef09dd210b3f23addd047f58b4
John <john.kmartins@gmail.com>
2020-03-18 19:11:28 -0300

    refact: rename a property in PlayerMock

--- Tests/Clappr_Tests/Classes/Mocks/PlayerMock.swift
+++ Tests/Clappr_Tests/Classes/Mocks/PlayerMock.swift
@@ -3,15 +3,15 @@ import AVFoundation
 class PlayerMock: AVPlayer {
     private var accessLogEvent: AccessLogEventMock
     private var isFinished: Bool
-    private var _playerItem: PlayerItemMock
+    private var playerItemMock: PlayerItemMock
     
     init(accessLogEvent: AccessLogEventMock, isFinished: Bool = false) {
         self.accessLogEvent = accessLogEvent
         self.isFinished = isFinished
-        self._playerItem = PlayerItemMock(accessLogEvent: accessLogEvent, isFinished: isFinished)
+        self.playerItemMock = PlayerItemMock(accessLogEvent: accessLogEvent, isFinished: isFinished)
             
         super.init()
     }
 
-    override var currentItem: AVPlayerItem? { _playerItem }
+    override var currentItem: AVPlayerItem? { playerItemMock }
 }
