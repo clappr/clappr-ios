75650c03c3bb412f1113eee7afb380d6f9ab4221
Paulo Santos <paulo.santos@thoughtworks.com>
2020-05-27 15:15:21 -0300

    feat: introduce updateFocus in UICorePlugin

--- Sources/Clappr/Classes/Plugin/Core/UICorePlugin.swift
+++ Sources/Clappr/Classes/Plugin/Core/UICorePlugin.swift
@@ -9,6 +9,16 @@ open class UICorePlugin: SimpleCorePlugin, UIPlugin {
         }
     }
     
+    #if os(tvOS)
+    func updateFocus(userInfo: EventUserInfo) {
+        core?.trigger(.requestFocusUpdate, userInfo: userInfo)
+    }
+    
+    func updateFocus() {
+        core?.trigger(.requestFocusUpdate)
+    }
+    #endif
+    
     open func render() {
         NSException(name: NSExceptionName("RenderNotOverriden"), reason: "UICorePlugins should always override the render method").raise()
     }
