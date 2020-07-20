1f1ed0a9a3a889ce132a2ce70e40394c03e16d4d
Victor Yves Crispim <vsilva@corp.globo.com>
2020-07-20 15:49:58 -0300

    test: adds BackgroundLayer size test

--- Tests/Clappr_Tests/Classes/Base/LayersCompositorTests.swift
+++ Tests/Clappr_Tests/Classes/Base/LayersCompositorTests.swift
@@ -8,6 +8,19 @@ class LayersCompositorTests: QuickSpec {
     override func spec() {
         describe(".LayersCompositor") {
             context("When LayersCompositor is attached to rootView") {
+                it("creates BackgroundLayer with the same size as rootView") {
+                    let frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+                    let rootView = UIView(frame: frame)
+                    
+                    let _ = LayersCompositor(rootView: rootView)
+                    
+                    if let backgroundLayer = rootView.subviews.first as? BackgroundLayer {
+                        expect(backgroundLayer.bounds.size).to(equal(rootView.bounds.size))
+                    } else {
+                        fail("BackgroundLayer is not the first subview of rootView)")
+                    }
+                }
+                
                 it("adds BackgroundLayer behind all other layers"){
                     let fakeLayer = FakeLayer()
                     let rootView = UIView()
