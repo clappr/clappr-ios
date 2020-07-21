d9842676a459f3517b75833196e91ebed350de27
Victor Yves Crispim <vsilva@corp.globo.com>
2020-07-21 15:48:39 -0300

    test: adds resize test to BackgroundLayer

--- Tests/Clappr_Tests/Classes/Base/BackgroundLayerTests.swift
+++ Tests/Clappr_Tests/Classes/Base/BackgroundLayerTests.swift
@@ -24,6 +24,21 @@ class BackgroundLayerTests: QuickSpec {
                     
                     expect(backgroundLayer.bounds.size).to(equal(rootView.bounds.size))
                 }
+                context("and the view size changes") {
+                    it("resizes to match the view bounds") {
+                        let frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+                        let biggerFrame = CGRect(x: 0, y: 0, width: 40, height: 40)
+                        let rootView = UIView(frame: frame)
+                        let backgroundLayer = BackgroundLayer()
+                        
+                        backgroundLayer.attach(to: rootView)
+                        backgroundLayer.layoutIfNeeded()
+                        rootView.bounds = biggerFrame
+                        rootView.layoutIfNeeded()
+                        
+                        expect(backgroundLayer.bounds.size).to(equal(biggerFrame.size))
+                    }
+                }
             }
         }
     }
