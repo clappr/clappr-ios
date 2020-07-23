3b71f1ef5d439bcfe14b4aebbe1a4721159d669e
Victor Yves Crispim <vsilva@corp.globo.com>
2020-07-23 17:59:54 -0300

    refactor: creates BackgroundLayer on declaration

--- Sources/Clappr_iOS/Classes/Base/Layers/LayersCompositor.swift
+++ Sources/Clappr_iOS/Classes/Base/Layers/LayersCompositor.swift
@@ -2,13 +2,11 @@ import UIKit
 
 class LayersCompositor {
     private weak var rootView: UIView?
-    private var backgroundLayer: BackgroundLayer?
+    private let backgroundLayer = BackgroundLayer()
     
     func compose(inside rootView: UIView) {
         self.rootView = rootView
         
-        let backgroundLayer = BackgroundLayer()
-        self.backgroundLayer = backgroundLayer
         backgroundLayer.attach(to: rootView)
         rootView.sendSubviewToBack(backgroundLayer)
     }
