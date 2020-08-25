ea74268d2b2ec89094030d275ad7766d8e0c0068
Victor Yves Crispim <vsilva@corp.globo.com>
2020-08-25 09:49:10 -0300

    feat: create PlaybackLayer

---
+++ Sources/Clappr/Classes/Base/Layers/PlaybackLayer.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class PlaybackLayer: UIView, Layer {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
