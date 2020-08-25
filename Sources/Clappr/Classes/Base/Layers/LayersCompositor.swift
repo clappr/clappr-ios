18226bc96136aedb6250574b11568cd435d70d88
Victor Yves Crispim <vsilva@corp.globo.com>
2020-08-25 10:04:19 -0300

    feat: add PlaybackLayer to hierarchy

--- Sources/Clappr/Classes/Base/Layers/LayersCompositor.swift
+++ Sources/Clappr/Classes/Base/Layers/LayersCompositor.swift
@@ -3,10 +3,12 @@ import UIKit
 class LayersCompositor {
     private weak var rootView: UIView?
     private let backgroundLayer = BackgroundLayer()
+    private let playbackLayer = PlaybackLayer()
     
     func compose(inside rootView: UIView) {
         self.rootView = rootView
         
         backgroundLayer.attach(to: rootView, at: 0)
+        playbackLayer.attach(to: rootView, at: 1)
     }
 }
