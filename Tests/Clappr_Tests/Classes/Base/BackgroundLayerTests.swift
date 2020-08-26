ea626561f32b767a402c6a251fc07af4003f23bd
Victor Yves Crispim <vsilva@corp.globo.com>
2020-08-26 15:36:02 -0300

    test: move Layer size and position tests

--- Tests/Clappr_Tests/Classes/Base/BackgroundLayerTests.swift
+++ Tests/Clappr_Tests/Classes/Base/BackgroundLayerTests.swift
@@ -13,33 +13,6 @@ class BackgroundLayerTests: QuickSpec {
                     expect(layer.backgroundColor).to(equal(UIColor.black))
                 }
             }
-            context("When BackgroundLayer is attached to a view") {
-                it("resizes to match the view bounds") {
-                    let frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-                    let rootView = UIView(frame: frame)
-                    let backgroundLayer = BackgroundLayer()
-                    
-                    backgroundLayer.attach(to: rootView)
-                    backgroundLayer.layoutIfNeeded()
-                    
-                    expect(backgroundLayer.bounds.size).to(equal(rootView.bounds.size))
-                }
-                context("and the view size changes") {
-                    it("resizes to match the view bounds") {
-                        let frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-                        let biggerFrame = CGRect(x: 0, y: 0, width: 40, height: 40)
-                        let rootView = UIView(frame: frame)
-                        let backgroundLayer = BackgroundLayer()
-                        
-                        backgroundLayer.attach(to: rootView)
-                        backgroundLayer.layoutIfNeeded()
-                        rootView.bounds = biggerFrame
-                        rootView.layoutIfNeeded()
-                        
-                        expect(backgroundLayer.bounds.size).to(equal(biggerFrame.size))
-                    }
-                }
-            }
         }
     }
 }
