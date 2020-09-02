9614ccf02307e4d9d30380cdaf81a0471999f494
Helbert Gomes <helbertgs@gmail.com>
2020-09-02 15:56:41 -0300

    refactor: save expected value before compare values

--- Tests/Clappr_Tests/Classes/Base/Layers/ContainerLayerTests.swift
+++ Tests/Clappr_Tests/Classes/Base/Layers/ContainerLayerTests.swift
@@ -10,12 +10,14 @@ class ContainerLayerTests: QuickSpec {
                     let containerView = UIView(frame: .zero)
                     let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
                     let layer = ContainerLayer(frame: frame)
+                    let expectedSize = frame.size
+                    let expectedCenter = layer.center
                     
                     layer.attachContainer(containerView)
                     layer.layoutIfNeeded()
                     
-                    expect(containerView.center).to(equal(layer.center))
-                    expect(containerView.bounds.size).to(equal(layer.bounds.size))
+                    expect(containerView.center).to(equal(expectedCenter))
+                    expect(containerView.bounds.size).to(equal(expectedSize))
                     
                 }
                 context("and the view size changes") {
@@ -24,13 +26,14 @@ class ContainerLayerTests: QuickSpec {
                         let bigFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
                         let containerView = UIView(frame: .zero)
                         let containerLayer = ContainerLayer(frame: smallFrame)
+                        let expectedSize = bigFrame.size
                         
                         containerLayer.attachContainer(containerView)
                         containerLayer.layoutIfNeeded()
                         containerLayer.bounds = bigFrame
                         containerLayer.layoutIfNeeded()
                         
-                        expect(containerView.bounds.size).to(equal(bigFrame.size))
+                        expect(containerView.bounds.size).to(equal(expectedSize))
                     }
                 }
             }
