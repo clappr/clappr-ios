f51bf41d81391383d18ec2fc2da0a421eacf55c8
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-10-05 14:25:50 -0300

    feat: Add passthrough on CoreLayer

--- Sources/Clappr/Classes/Base/Layers/CoreLayer.swift
+++ Sources/Clappr/Classes/Base/Layers/CoreLayer.swift
@@ -4,4 +4,10 @@ final class CoreLayer: Layer {
     func attachPlugin(_ plugin: UICorePlugin) {
         addSubview(plugin.view)
     }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let result = super.hitTest(point, with: event)
+        if result == self { return nil }
+        return result
+    }
 }
