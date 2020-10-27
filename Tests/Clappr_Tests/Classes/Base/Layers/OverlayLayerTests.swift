60e6d6d03fedc24843522e2dd93bbe81a84bbe34
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-10-27 09:37:12 -0300

    test: create OverlayLayerTests file

---
+++ Tests/Clappr_Tests/Classes/Base/Layers/OverlayLayerTests.swift
@@ -0,0 +1,11 @@
+import Quick
+import Nimble
+@testable import Clappr
+
+class OverlayLayerTests: QuickSpec {
+    override func spec() {
+        describe(".OverlayLayerTests"){
+            
+        }
+    }
+}
