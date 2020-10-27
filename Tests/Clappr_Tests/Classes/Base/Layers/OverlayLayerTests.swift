2e8adea1ef3f78c638202ae0315cb50b2546532f
Victor Yves Crispim <rotcivyves@hotmail.com>
2020-10-27 15:23:27 -0300

    test: test attachOverlay method

--- Tests/Clappr_Tests/Classes/Base/Layers/OverlayLayerTests.swift
+++ Tests/Clappr_Tests/Classes/Base/Layers/OverlayLayerTests.swift
@@ -5,7 +5,35 @@ import Nimble
 class OverlayLayerTests: QuickSpec {
     override func spec() {
         describe(".OverlayLayerTests"){
-            
+            context("When overlay view is attached") {
+                it("resizes to match the layer bounds") {
+                    let overlayView = UIView(frame: .zero)
+                    let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+                    let layer = OverlayLayer(frame: frame)
+                    let expectedSize = frame.size
+                    let expectedCenter = layer.center
+                    
+                    layer.attachOverlay(overlayView)
+                    
+                    expect(overlayView.center).to(equal(expectedCenter))
+                    expect(overlayView.bounds.size).to(equal(expectedSize))
+                }
+                context("and the view size changes") {
+                    it("resizes to match the view bounds") {
+                        let smallFrame = CGRect(x: 0, y: 0, width: 50, height: 50)
+                        let bigFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+                        let overlayView = UIView(frame: .zero)
+                        let layer = OverlayLayer(frame: smallFrame)
+                        let expectedSize = bigFrame.size
+                        
+                        layer.attachOverlay(overlayView)
+                        layer.frame = bigFrame
+                        layer.layoutIfNeeded()
+                        
+                        expect(overlayView.bounds.size).to(equal(expectedSize))
+                    }
+                }
+            }
         }
     }
 }
